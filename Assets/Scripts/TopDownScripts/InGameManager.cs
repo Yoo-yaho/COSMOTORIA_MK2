6c323e7689bc2eb4b5c37f4e6d7c48844782404e
Yoo-yaho <dnjsldkqjwl@naver.com>
2022-11-09 02:08:39 +0900

    인물 대화창 수정
    20221109

--- Assets/Scripts/TopDownScripts/InGameManager.cs
+++ Assets/Scripts/TopDownScripts/InGameManager.cs
@@ -7,7 +7,10 @@ public class InGameManager : MonoBehaviour
 {
     public float textSpeed;
     public bool isAction = false;
+    public int talkIndex;
 
+    public Image portraitImage;
+    public TalkManager talkManager;
     public Text talkText;
     public GameObject scanObject;
     public GameObject talkPanel;
@@ -18,18 +21,48 @@ public class InGameManager : MonoBehaviour
     }
 
     public void Scan(GameObject scanObj)
-    {
-        if (isAction) // 대화창 나갈 때
-        {
-            isAction = false;
-        }
-        else // 대화창 나올 때
-        {
-            isAction = true;
-            scanObject = scanObj;
-            talkText.text = "이것은 " + scanObject.name + "이다.";
-        }
+    {      
+        scanObject = scanObj;
+        ObjectData objectData = scanObject.GetComponent<ObjectData>();
+        Talk(objectData.id, objectData.isNpc);
 
         talkPanel.SetActive(isAction);
+    }
+
+    void Talk(int id, bool isNpc)
+    {
+        string talkData = talkManager.GetTalk(id, talkIndex);
+
+        if (talkData == null)
+        {
+            //talkPanel.GetComponent<Animator>().SetTrigger("isText");
+            isAction = false;
+            talkIndex = 0;
+            return;
+        }
+
+        if (isNpc)
+        {
+            //talkPanel.GetComponent<Animator>().SetTrigger("isText");
+            talkText.text = talkData.Split(':')[0];
+
+            portraitImage.sprite = talkManager.GetPortrait(id, int.Parse(talkData.Split(':')[1]));
+            portraitImage.color = new Color(1, 1, 1, 1);
+        } 
+        else
+        {
+            //talkPanel.GetComponent<Animator>().SetTrigger("isText");
+            talkText.text = talkData;
+
+            portraitImage.color = new Color(1, 1, 1, 0);
+        }
+
+        isAction = true;
+        talkIndex++;
+    }
+
+    public void TalkEnd()
+    {
+        isAction = false;
     }
 }
